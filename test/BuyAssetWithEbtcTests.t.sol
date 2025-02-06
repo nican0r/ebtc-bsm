0ef8fd504afa13d8f1bc0bb1aea2c61e5dbeee49
JulissaDantes <julissadcj@gmail.com>
2025-02-06 14:10:40 -0400

    adding tests for events

--- test/BuyAssetWithEbtcTests.t.sol
+++ test/BuyAssetWithEbtcTests.t.sol
@@ -6,6 +6,9 @@ import {IEbtcBSM} from "../src/Dependencies/IEbtcBSM.sol";
 
 contract BuyAssetWithEbtcTests is BSMTestBase {
 
+    event BoughtAssetWithEbtc(uint256 ebtcAmountIn, uint256 assetAmountOut, uint256 feeAmount);
+    event FeeToBuyAssetUpdated(uint256 oldFee, uint256 newFee);
+
     function testBuyAssetSuccess() public {
         vm.prank(testMinter);
         bsmTester.buyEbtcWithAsset(5e18);
@@ -13,10 +16,15 @@ contract BuyAssetWithEbtcTests is BSMTestBase {
         assertEq(mockAssetToken.balanceOf(testBuyer), 0);
         assertEq(mockEbtcToken.balanceOf(testBuyer), 10e18);
 
-        // TODO: test events
+        vm.recordLogs();
         vm.prank(testBuyer);
-        assertEq(bsmTester.buyAssetWithEbtc(3e18), 3e18);
 
+        assertEq(bsmTester.buyAssetWithEbtc(3e18), 3e18);
+        Vm.Log[] memory entries = vm.getRecordedLogs();
+
+        assertEq(entries[0].topics[0], keccak256("Transfer(address,address,uint256)"));
+        assertEq(entries[1].topics[0], keccak256("Transfer(address,address,uint256)"));
+        assertEq(entries[2].topics[0], keccak256("BoughtAssetWithEbtc(uint256,uint256,uint256)"));
         assertEq(mockAssetToken.balanceOf(testBuyer), 3e18);
         assertEq(mockEbtcToken.balanceOf(testBuyer), 7e18);
     }
@@ -24,17 +32,26 @@ contract BuyAssetWithEbtcTests is BSMTestBase {
     function testBuyAssetFee() public {
         // 1% fee
         vm.prank(techOpsMultisig);
+        vm.expectEmit(false, true, false, false);
+        emit FeeToBuyAssetUpdated(0, 100);
         bsmTester.setFeeToBuyAsset(100);
 
+        vm.recordLogs();
         vm.prank(testMinter);
         bsmTester.buyEbtcWithAsset(5e18);
 
+        Vm.Log[] memory entries = vm.getRecordedLogs();
+
+        assertEq(entries[0].topics[0], keccak256("Transfer(address,address,uint256)"));
+        assertEq(entries[1].topics[0], keccak256("Transfer(address,address,uint256)"));
+        assertEq(entries[2].topics[0], keccak256("BoughtEbtcWithAsset(uint256,uint256,uint256)"));
         assertEq(mockAssetToken.balanceOf(testBuyer), 0);
         assertEq(mockEbtcToken.balanceOf(testBuyer), 10e18);
 
-        // TODO: test events
-
         vm.prank(testBuyer);
+        vm.expectEmit(true, false, false, false);
+        emit BoughtAssetWithEbtc(1e18, 0, 0);
+
         assertEq(bsmTester.buyAssetWithEbtc(1e18), 0.99e18);
 
         assertEq(mockAssetToken.balanceOf(testBuyer), 0.99e18);
