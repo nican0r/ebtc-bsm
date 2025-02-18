406bdc0407705d6b36a234bc1969760cb8a95450
JulissaDantes <julissadcj@gmail.com>
2025-02-18 12:49:55 -0400

    removing extra observer

--- scripts/ObserveCompare.s.sol
+++ scripts/ObserveCompare.s.sol
@@ -14,9 +14,8 @@ contract ObserveCompare is Script {
     address constant activePoolAddress = 0x6dBDB6D420c110290431E863A1A978AE53F69ebC;
     address constant ebtc = 0x661c70333AA1850CcDBAe82776Bb436A0fCfeEfB;
     IActivePoolObserver activePool = IActivePoolObserver(activePoolAddress);
-    ITwapWeightedObserver observer;
     // Amount of blocks to consider
-    uint256 blocksToCheck = 50;
+    uint256 blocksToCheck = 500000;
     // Semi random way of checking blocks
     uint256 step = 10;
 
@@ -24,15 +23,14 @@ contract ObserveCompare is Script {
         string memory rpcUrl = vm.envString("RPC_URL");
         uint256 forkId = vm.createFork(rpcUrl);
         vm.selectFork(forkId);
-        //uint256 deployBlock = 19437199; // ActivePool was deployed at this block
+
         uint256 endBlock = block.number; 
-        uint256 startBlock = endBlock - blocksToCheck;//ActivePool must had been deployed before this
+        uint256 startBlock = endBlock - blocksToCheck;// Must be greater than 19437199, the ActivePool deployment block
         uint256 counter;
 
         vm.roll(startBlock - 1);
-        observer = new TwapWeightedObserver(0);
-        ActivePoolObserver activePoolObserver = new ActivePoolObserver(ITwapWeightedObserver(address(observer)));
-        console.log("About to compare...");
+        ActivePoolObserver activePoolObserver = new ActivePoolObserver(ITwapWeightedObserver(address(activePool)));
+        console.log("Starting...");
         for(uint256 i = startBlock; i <= endBlock; i += step){
             vm.roll(i);
             
