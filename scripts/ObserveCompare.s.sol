f3d5aa9de292f3c9b130a50edb5f0bbb759c05f2
JulissaDantes <julissadcj@gmail.com>
2025-02-18 09:01:18 -0400

    updating script

--- scripts/ObserveCompare.s.sol
+++ scripts/ObserveCompare.s.sol
@@ -9,22 +9,27 @@ import {ITwapWeightedObserver} from "../src/Dependencies/ITwapWeightedObserver.s
 import {TwapWeightedObserver} from "../src/Dependencies/TwapWeightedObserver.sol";
 
 // Must have the ETH_RPC_URL environment variable set
+// forge script scripts/ObserveCompare.s.sol:ObserveCompare --fork
 contract ObserveCompare is Script {
     address constant activePoolAddress = 0x6dBDB6D420c110290431E863A1A978AE53F69ebC;
     address constant ebtc = 0x661c70333AA1850CcDBAe82776Bb436A0fCfeEfB;
     IActivePoolObserver activePool = IActivePoolObserver(activePoolAddress);
     ITwapWeightedObserver observer;
+    uint256 blocksToCheck = 5;
 
     function run() external {
-        vm.createFork("mainnet"); // Create a fork of mainnet
-        //uint256 startBlock = 19437199; // ActivePool was deployed at this block
+        string memory rpcUrl = vm.envString("RPC_URL");
+        uint256 forkId = vm.createFork(rpcUrl);
+        vm.selectFork(forkId);
+        //uint256 deployBlock = 19437199; // ActivePool was deployed at this block
         uint256 endBlock = block.number; 
-        uint256 startBlock = endBlock - 49000;
+        uint256 startBlock = endBlock - blocksToCheck;//ActivePool must had been deployed before this
+        uint256 counter;
 
         vm.roll(startBlock - 1);
         observer = new TwapWeightedObserver(0);
         ActivePoolObserver activePoolObserver = new ActivePoolObserver(ITwapWeightedObserver(address(observer)));
-
+        console.log("About to compare...");
         for(uint256 i = startBlock; i <= endBlock; i += 1){//TODO customize + 1
             vm.roll(i);
             
@@ -32,9 +37,12 @@ contract ObserveCompare is Script {
             uint256 resultObserver = activePoolObserver.observe();
 
             if(resultActivePool != resultObserver) {
-                console.log("Mismatch at: ", i);
+                console.log("Mismatch at block: ", i);
+                console.log("\t Deployed AP value: ", resultActivePool, " != APObserver: ", resultObserver);
+                counter++;
             }
         }
+        console.log("Finished comparison with a total of ", counter, " different results, out of ", blocksToCheck);
     }
 
 }
