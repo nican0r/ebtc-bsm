67b2653d4381681f66d7f24565d160ae918dd388
gallo <alex@entreprenerd.xyz>
2025-02-20 12:42:24 +0100

    chore: clean up

--- test/recon/CryticToFoundry.sol
+++ test/recon/CryticToFoundry.sol
@@ -52,40 +52,6 @@ contract CryticToFoundry is Test, TargetFunctions, FoundryAsserts {
         property_assets_are_not_lost();
     }
 
-    // forge test --match-test test_inlined_withdrawProfitTest_123 -vvv
-    function test_inlined_withdrawProfitTest_123() public {
-        switch_asset(1);
-
-        asset_mint(0xD6BbDE9174b1CdAa358d2Cf4D57D1a9F7178FBfF, 1);
-
-        asset_mint(0xc7183455a4C133Ae270771860664b6B7ec320bB1, 1);
-
-        uint256 amt = escrow.feeProfit();
-        console2.log(
-            "escrow.EXTERNAL_VAULT().balanceOf(address(escrow));", escrow.EXTERNAL_VAULT().balanceOf(address(escrow))
-        );
-
-        uint256 balB4Escrow = escrow.totalBalance();
-
-        // Expected lower
-        uint256 shares = escrow.EXTERNAL_VAULT().convertToShares(amt);
-        uint256 expected = escrow.EXTERNAL_VAULT().previewRedeem(shares);
-
-        console2.log("amt", amt);
-        console2.log("balB4Escrow", balB4Escrow);
-        console2.log("shares", shares);
-        console2.log("expected", expected);
-
-        uint256 balB4 = (escrow.ASSET_TOKEN()).balanceOf(address(escrow.FEE_RECIPIENT()));
-
-        inlined_withdrawProfitTest();
-
-        uint256 balAfter = (escrow.ASSET_TOKEN()).balanceOf(address(escrow.FEE_RECIPIENT()));
-
-        console2.log("balB4", balB4);
-        console2.log("balAfter", balAfter);
-    }
-
     // forge test --match-test test_property_fees_profit_increases_3 -vvv
     function test_property_fees_profit_increases_3() public {
         switch_asset(1);
