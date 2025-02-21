37f48ea8ef604552832f935cd9819b4516291af2
JulissaDantes <julissadcj@gmail.com>
2025-02-21 16:02:55 -0400

    creating forking test with real data

--- test/ChainlinkAdapterTests.t.sol
+++ test/ChainlinkAdapterTests.t.sol
@@ -2,13 +2,13 @@
 pragma solidity ^0.8.25;
 
 import "forge-std/Test.sol";
-import {ChainlinkAdapter} from "../src/ChainlinkAdapter.sol";
+import {ChainlinkAdapter, AggregatorV3Interface} from "../src/ChainlinkAdapter.sol";
 import {MockAssetOracle} from "./mocks/MockAssetOracle.sol";
 
 
 contract ChainlinkAdapterTests is Test {
 
-    MockAssetOracle internal usdtBtcAggregator;//TODO I think this is not right
+    MockAssetOracle internal usdtBtcAggregator;
     MockAssetOracle internal btcUsdAggregator;
     ChainlinkAdapter internal chainlinkAdapter;
 
@@ -46,14 +46,10 @@ contract ChainlinkAdapterTests is Test {
         // tBTC > BTC
         // tBTC
         usdtBtcAggregator.setLatestRoundId(1);
-        usdtBtcAggregator.setPrevRoundId(110680464442257320246);
         usdtBtcAggregator.setPrice(2);
-        usdtBtcAggregator.setUpdateTime(1706208946);
         // BTC
         btcUsdAggregator.setLatestRoundId(1);
-        btcUsdAggregator.setPrevRoundId(110680464442257320664);
         btcUsdAggregator.setPrice(1);// half the price
-        btcUsdAggregator.setUpdateTime(1706208947);
 
         (
             ,
@@ -65,15 +61,12 @@ contract ChainlinkAdapterTests is Test {
         // tBTC < BTC
         // tBTC
         usdtBtcAggregator.setLatestRoundId(1);
-        usdtBtcAggregator.setPrevRoundId(110680464442257320246);
         usdtBtcAggregator.setPrice(1);
-        usdtBtcAggregator.setUpdateTime(1706208946);
+        
         // BTC
-        btcUsdAggregator.setLatestRoundId(1);
-        btcUsdAggregator.setPrevRoundId(110680464442257320664);
+        btcUsdAggregator.setLatestRoundId(1);        
         btcUsdAggregator.setPrice(2);// double the price
-        btcUsdAggregator.setUpdateTime(1706208947);
-
+        
         (
             ,
             answer,,,
@@ -82,7 +75,19 @@ contract ChainlinkAdapterTests is Test {
         assertEq(answer, 2000000000000000000);// (tBTC/BTC) * ADAPTER_PRECISION
     }
 
-    function testWithReaData() public {
-        // test that reading the prices work by rolling to a block in mainnet that actually we know the prices and confirm the conversion works
+    function testWithRealData() public {
+        string memory rpcUrl = vm.envString("RPC_URL");
+        uint256 forkId = vm.createFork(rpcUrl);
+        vm.selectFork(forkId);
+        // test that reading the prices work by rolling to a block in mainnet that actually 
+        // we know the prices and confirm the conversion works
+        address tBtcUsdFeed = 0x8350b7De6a6a2C1368E7D4Bd968190e13E354297;
+        address btcUsdFeed = 0xF4030086522a5bEEa4988F8cA5B36dbC97BeE88c;
+        
+        ChainlinkAdapter adapter = new ChainlinkAdapter(AggregatorV3Interface(tBtcUsdFeed), AggregatorV3Interface(btcUsdFeed));
+        
+        (uint80 roundID, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) = adapter.latestRoundData();
+        emit log_named_int("Converted tBTC to BTC price", answer);
+        assertTrue(answer > 0, "Conversion should yield a positive number");
     }
 }
